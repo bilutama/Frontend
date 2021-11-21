20342765e9d7975e03b2d0c96b270f891f6829e3
Maksim Biluta <biluta.m.a@gmail.com>
2021-11-21 22:19:06 +0100

    Js_arrays

--- Js_arrays/arrays.js
+++ Js_arrays/arrays.js
@@ -11,10 +11,49 @@
 // Оформите код в виде функций
 
 (function () {
-    var arr = [1, 3, 4, 5, 6, 9, 0, 17, 20, 2, 56, 31, 15];
-    arr.sort(function (x, y) {
+    var array = [1, 3, 4, 5, 6, 9, 0, 17, 20, 2, 56, 31, 15];
+    array.sort(function (x, y) {
         return y - x;
     });
 
-    console.log("Descending sorted array: " + arr.join(", "));
+    console.log("Descending sorted array: " + array.join(", "));
+
+    var elementsCount = 5;
+
+    var firstFiveElements = array.slice(0, elementsCount);
+    console.log("First five elements: " + firstFiveElements.join(", "));
+
+    var lastFiveElements = array.slice(array.length >= elementsCount ? array.length - elementsCount : 0);
+    console.log("Last five elements: " + lastFiveElements.join(", "));
+
+    var evenElementsSum;
+
+    evenElementsSum = array.reduce(function (accumulator, element) {
+        accumulator += element % 2 === 0 ? element : 0;
+        return accumulator;
+    }, 0);
+
+    console.log("Sum of even elements of array: " + evenElementsSum);
+
+    var newArrayLength = 100;
+    var oneHundredElementsArray = [];
+
+    for (var i = 1; i <= newArrayLength; ++i) {
+        oneHundredElementsArray.push(i);
+    }
+
+    console.log("new array: " + oneHundredElementsArray.join(", "));
+
+    var filterEvenNumbers = function (element) {
+        if (element % 2 === 0) {
+            return element;
+        }
+    }
+
+    var getNumberSquare = function (element) {
+        return Math.pow(element, 2);
+    }
+
+    var evenElementsSquares = oneHundredElementsArray.filter(filterEvenNumbers).map(getNumberSquare);
+    console.log("Even squares: " + evenElementsSquares.join(", "));
 })()
