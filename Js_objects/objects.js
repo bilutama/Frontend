fc1876d64abc1896f86a2c85943ae002e12f92fb
bilutama <biluta.m.a@gmail.com>
2021-11-22 18:16:07 +0100

    js_objects

--- Js_objects/objects.js
+++ Js_objects/objects.js
@@ -1,3 +1,106 @@
 (function () {
-    var array = [];
+    var countries = [
+        {
+            country: "Germany",
+            cities: [
+                {
+                    city: "Berlin",
+                    population: 3275000
+                },
+                {
+                    city: "Hamburg",
+                    population: 1686100
+                },
+                {
+                    city: "Munich",
+                    population: 1185400
+                },
+                {
+                    city: "Cologne",
+                    population: 965300
+                }]
+        },
+        {
+            country: "France",
+            cities: [
+                {
+                    city: "Paris",
+                    population: 2187526
+                },
+                {
+                    city: "Marseille",
+                    population: 863310
+                },
+                {
+                    city: "Lyon",
+                    population: 516092
+                },
+                {
+                    city: "Toulouse",
+                    population: 479553
+                }]
+        },
+        {
+            country: "UK",
+            cities: [
+                {
+                    city: "London",
+                    population: 9304016
+                },
+                {
+                    city: "Manchester",
+                    population: 2730076
+                },
+                {
+                    city: "Birmingham",
+                    population: 2607437
+                },
+                {
+                    city: "Leeds",
+                    population: 1889095
+                }]
+        },
+        {
+            country: "Italy",
+            cities: [
+                {
+                    city: "Rome",
+                    population: 2318895
+                },
+                {
+                    city: "Milan",
+                    population: 1236837
+                },
+                {
+                    city: "Naples",
+                    population: 959470
+                },
+                {
+                    city: "Turin",
+                    population: 870456
+                }]
+        },
+        {
+            country: "Spain",
+            cities: [
+                {
+                    city: "Madrid",
+                    population: 3266126
+                },
+                {
+                    city: "Barcelona",
+                    population: 1636762
+                },
+                {
+                    city: "Valencia",
+                    population: 794288
+                },
+                {
+                    city: "Seville",
+                    population: 688592
+                }]
+        },
+    ];
+
+
 })()
