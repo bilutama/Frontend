614e1c090d9acaa1d7439bee3ab607bff556c9c0
Maksim Biluta <biluta.m.a@gmail.com>
2021-11-28 02:46:34 +0100

    temperature

--- Temperature/converter.js
+++ Temperature/converter.js
@@ -2,7 +2,8 @@
     "use strict";
 
     var input_field = document.getElementById("input_field");
-    var output_field = document.getElementById("output_field");
+    var output_field_kelvins = document.getElementById("output_field_kelvins");
+    var output_field_fahrenheits = document.getElementById("output_field_fahrenheits");
     var convert_button = document.getElementById("convert_button");
 
     input_field.addEventListener("focus", function () {
@@ -11,7 +12,8 @@
 
     convert_button.addEventListener("click", function () {
         var input_temperature = input_field.value.trim();
-        output_field.value = "";
+        output_field_kelvins.value = "";
+        output_field_fahrenheits.value = "";
 
         if (input_temperature === "") {
             return;
@@ -27,6 +29,7 @@
         }
 
         input_field.value = input_temperature;
-        output_field.value = (input_temperature * 9 / 5 + 32).toFixed(2);
+        output_field_kelvins.value = (Number(input_temperature) + 273.15).toFixed(2);
+        output_field_fahrenheits.value = (input_temperature * 9 / 5 + 32).toFixed(2);
     });
 })();
