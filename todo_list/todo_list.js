8bd7dc50f0c8db6e89ff1d48a853eb4ad709bc3d
Maksim Biluta <biluta.m.a@gmail.com>
2021-12-01 00:53:31 +0100

    todo-list

---
+++ todo_list/todo_list.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var todoList = document.getElementById("todo-list_content");
+    var newNote = document.getElementById("new_note");
+    var saveNewNoteButton = document.getElementById("save_note_button");
+
+    saveNewNoteButton.addEventListener("click", function () {
+        var text = newNote.value.trim();
+
+        if (text === "") {
+            return
+        }
+
+        var newTodoListItem = document.createElement("li");
+        newTodoListItem.innerHTML = "<span class='todo_note'></span><button class='delete_button' type='button'>";
+        newTodoListItem.querySelector(".todo_note").textContent = text;
+
+        todoList.appendChild(newTodoListItem);
+        newNote.value = "";
+
+        var deleteButton = newTodoListItem.querySelector(".delete_button");
+        deleteButton.addEventListener("click", function (){
+            newTodoListItem.remove();
+        });
+    });
+});
