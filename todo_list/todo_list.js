2d66336297c30d1314b86bf0830efc8a987631d2
Maksim Biluta <biluta.m.a@gmail.com>
2021-12-04 17:33:41 +0100

    todo-list

--- todo_list/todo_list.js
+++ todo_list/todo_list.js
@@ -1,52 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
     var todoList = document.getElementById("todo-list_content");
-    var newNote = document.getElementById("new_note");
-    var saveNoteButton = document.getElementById("save_note_button");
+    var newTodoNote = document.getElementById("new_note");
+    var addNewNoteButton = document.getElementById("add_new_note_button");
 
-    saveNoteButton.addEventListener("click", function () {
-        var text = newNote.value.trim();
+    addNewNoteButton.addEventListener("click", function () {
+        var text = newTodoNote.value.trim();
 
         if (text.length === 0) {
             return
         }
 
-        var newTodoItem = document.createElement("li");
+        var todoNote = document.createElement("li");
 
         function setEditMode() {
-            newTodoItem.innerHTML = "<input class='edit_note'>"
-                + "<button class='save_button' type='button'>save</button>"
-                + "<button class='cancel_button' type='button'>cancel</button>";
+            todoNote.innerHTML = "<div class='note_block'><input class='edit_note'></div>"
+                + "<div class='note_block'><input class='save_button' type='image' src='resources/save.png' alt='save' height='32' width='32'></div>";
 
-            newTodoItem.querySelector(".edit_note").value = text;
+            todoNote.querySelector(".edit_note").value = text;
+            todoNote.querySelector(".edit_note").focus();
 
-            newTodoItem.querySelector(".save_button").addEventListener("click", function () {
-                text = newTodoItem.querySelector(".edit_note").value;
+            todoNote.querySelector(".save_button").addEventListener("mousedown", function () {
+                text = todoNote.querySelector(".edit_note").value;
                 setViewMode();
             });
 
-            newTodoItem.querySelector(".cancel_button").addEventListener("click", function () {
-                setViewMode();
+            todoNote.querySelector(".edit_note").addEventListener("blur", function () {
+                //setViewMode();
             });
         }
 
         function setViewMode() {
-            newTodoItem.innerHTML = "<span class='todo_note'></span>"
-                + "<button class='edit_button' type='image' src=\"resources/edit.png\">edit</button>"
-                + "<button class='delete_button' type='button'>delete</button>";
+            todoNote.innerHTML = "<div class='note_block'><span class='todo_note'></span></div>"
+                + "<div class='note_block'><input class='edit_button' type='image' alt='edit' src='resources/edit.png'  height='26' width='26'></div>"
+                + "<div class='note_block'><input class='delete_button' type='image' alt='delete' src='resources/remove.png'  height='26' width='26'></div>";
 
-            newTodoItem.querySelector(".todo_note").textContent = text;
+            todoNote.querySelector(".todo_note").textContent = text;
 
-            newTodoItem.querySelector(".delete_button").addEventListener("click", function () {
-                newTodoItem.remove();
+            todoNote.querySelector(".delete_button").addEventListener("click", function () {
+                todoNote.remove();
             });
 
-            newTodoItem.querySelector(".edit_button").addEventListener("click", function () {
+            todoNote.querySelector(".todo_note").addEventListener("click", function () {
+                setEditMode();
+            });
+
+            todoNote.querySelector(".edit_button").addEventListener("click", function () {
                 setEditMode();
             });
         }
 
         setViewMode();
-        todoList.appendChild(newTodoItem);
-        newNote.value = "";
+        todoList.appendChild(todoNote);
+        newTodoNote.value = "";
     });
 });
