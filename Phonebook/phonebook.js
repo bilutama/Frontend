6cfcf05d7b00c9aeea6bb9714fb4074d7e8cc434
Maksim Biluta <biluta.m.a@gmail.com>
2021-12-20 01:51:02 +0100

    phonebook

--- Phonebook/phonebook.js
+++ Phonebook/phonebook.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
-    var contactInputForm = $("#contactInputForm");
+    var contactInputForm = $("#contact_input_form");
 
-    var firstName = $("#firstName");
-    var lastName = $("#lastName");
-    var telephoneNumber = $("#telephoneNumber");
+    var firstName = $("#first_name");
+    var lastName = $("#last_name");
+    var telephoneNumber = $("#telephone_number");
 
-    var addContactButton = $("#addContactButton");
+    var addContactButton = $("#add_contact_button");
     var phonebookContent = $("#phonebook").children("tbody");
 
-    var deleteContactDialog = $("#deleteConfirmationModal");
+    var deleteContactDialog = $("#delete_confirmation_modal");
+    var generalCheckbox = $("#general_checkbox");
     var contactToDelete = null;
 
     function getCapitalizedString(string) {
@@ -45,41 +46,52 @@ $(document).ready(function () {
 
         var newContact = $("<tr>");
 
-        newContact.html("<th scope='row' class='rowNumber'></th>" +
-            "<td class='contactFirstName'></td>" +
-            "<td class='contactLastName'></td>" +
-            "<td class='contactTelephone'></td>" +
-            "<td><button type='button' class='btn-close' aria-label='Delete' data-bs-toggle='modal' data-bs-target='#deleteConfirmationModal'></button></td>"
+        newContact.html("<th class='contactSelector'><div class='form-check'><input class='form-check-input' type='checkbox' value=''></div></th>" +
+            "<th scope='row' class='row_number'></th>" +
+            "<td class='contact_first_name'></td>" +
+            "<td class='contact_last_name'></td>" +
+            "<td class='contact_telephone'></td>" +
+            "<td><button type='button' class='btn-close' aria-label='Delete' data-bs-toggle='modal' data-bs-target='#delete_confirmation_modal'></button></td>"
         );
 
         var currentContactsCount = phonebookContent.children().length;
 
-        newContact.find(".rowNumber").text(currentContactsCount + 1);
-        newContact.find(".contactFirstName").text(getCapitalizedString(firstName.val()));
-        newContact.find(".contactLastName").text(getCapitalizedString(lastName.val()));
-        newContact.find(".contactTelephone").text(telephoneNumber.val());
+        newContact.find(".row_number").text(currentContactsCount + 1);
+        newContact.find(".contact_first_name").text(getCapitalizedString(firstName.val()));
+        newContact.find(".contact_last_name").text(getCapitalizedString(lastName.val()));
+        newContact.find(".contact_telephone").text(telephoneNumber.val());
 
         // Handler for contact removing
-        newContact.find(".btn-close").click(function () {
+        newContact.find(".btn-close").click(function (event) {
+            event.stopPropagation();
+
             contactToDelete = newContact;
             deleteContactDialog.show();
             $(".modal-body").text("Delete contact " +
-                contactToDelete.find(".contactFirstName").text() +
+                contactToDelete.find(".contact_first_name").text() +
                 " " +
-                contactToDelete.find(".contactLastName").text() +
+                contactToDelete.find(".contact_last_name").text() +
                 "?");
         });
 
+        newContact.find(".form-check-input").click(function (event) {
+            event.stopPropagation();
+        });
+
         phonebookContent.append(newContact);
+        // TODO: update general_checkbox status
 
         clearForm(contactInputForm);
         contactInputForm.removeClass("was-validated");
     }
 
-    $(document).on("shown.bs.modal", "#deleteConfirmationModal", function () {
+    addContactButton.click(addContactHandler);
+
+    // Modal dialog to confirm contact delete
+    $(document).on("shown.bs.modal", "#delete_confirmation_modal", function () {
         // Dialog dismiss is automated by using attribute _data-bs-dismiss_ for modal buttons
         // Thus there is no need for _cancel_ button handler
-        // and call _hide()_ method when confirm delete
+        // and call _hide()_ method when delete is confirmed
 
         // Modal dialog DELETE button handler
         $("#modal-delete").click(function () {
@@ -87,10 +99,34 @@ $(document).ready(function () {
 
             // Recalculate remaining contacts numbers
             phonebookContent.children("tr").each(function (index) {
-                $(this).find(".rowNumber").text(index + 1);
+                $(this).find(".row_number").text(index + 1);
             });
-        });
-    })
 
-    addContactButton.click(addContactHandler);
+            // TODO: update general_checkbox status
+        });
+    });
+
+    // GENERAL_CHECKBOX status handling on change
+    generalCheckbox.change(function (event) {
+        //event.stopPropagation();
+
+        if ($(this).is(":checked")) {
+            phonebookContent.children("tr").each(function () {
+                $(this).find(".form-check-input").prop('checked', true);
+            });
+        } else if ($(this).is(":not(:checked)")) {
+            phonebookContent.children("tr").each(function () {
+                $(this).find(".form-check-input").prop('checked', false);
+            });
+        }
+
+        console.log("MAIN CHECKBOX CLICKED");
+    });
+
+    phonebookContent.delegate("tr", "click", function (index, row) {
+        var checkbox = $(this).find(".form-check-input");
+        checkbox.prop('checked', !checkbox.prop('checked'));
+        console.log("ROW CLICKED");
+        return false;
+    });
 });
