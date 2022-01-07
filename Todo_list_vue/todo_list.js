a3c1f837826df8aea7656d23a52f178edefab77c
bilutama <Borntobewild282>
2022-01-07 10:26:37 +0700

    todo-list

--- Todo_list_vue/todo_list.js
+++ Todo_list_vue/todo_list.js
@@ -1,107 +1,85 @@
-new Vue({
-    el: "#app",
-    data: {
-        name: "",
-        surname: "",
-        fullName: ""
+Vue.component("to-do-item", {
+    template: "#to-do-item-template",
+
+    props: {
+        index: {
+            type: Number,
+            required: true
+        },
+
+        item: {
+            type: Object,
+            required: true,
+        }
     },
+
+    data: {
+        isInvalid: false,
+        isEditing: false,
+        editingText: ""
+    },
+
     methods: {
-        getFullName: function () {
-            this.fullName = this.name + " " + this.surname
+        editItem: function () {
+            this.isInvalid = false;
+            this.isEditing = true;
+            this.editingText = this.text;
+        },
+
+        delete: function () {
+            this.$emit("delete-item", this.props.item);
+        },
+
+        saveItem: function () {
+            this.isInvalid = false;
+
+            if (this.editingText.length === 0) {
+                this.isInvalid = true;
+                return;
+            }
+
+            this.$emit("save-item", this.item, this.editingText);
+            this.isEditing = false;
         }
     }
 });
 
-/*
-$(document).ready(function () {
-    var todoList = $("#todo-list_content");
-    var newTodoNote = $("#new_note");
-    var addNewNoteButton = $("#add_new_note_button");
+new Vue({
+    el: "#app",
 
-    newTodoNote.click(function () {
-        $("span.error_message").hide();
-    });
+    data: {
+        isNewTodoInvalid: false,
+        newTodoText: "",
+        currentTodoId: 1,
+        items: []
+    },
 
-    function addNewNoteHandler() {
-        var text = newTodoNote.val().trim();
+    methods: {
+        addNewTodoItem: function () {
+            this.isNewTodoInvalid = false;
 
-        if (text.length === 0) {
-            $("span.error_message").show();
-            return;
-        }
+            if (this.newTodoText.length === 0) {
+                this.isNewTodoInvalid = true;
+                return;
+            }
 
-        var todoNote = $("<li>");
-
-        function setEditMode() {
-            todoNote.html("<div class='note_block'>" +
-                "<textarea class='edit_note'></textarea>" +
-                "<span class='button_group'>" +
-                "<button class='list_button note_button save_button' type='button'>save</button>" +
-                "<button class='list_button note_button cancel_button' type='button'>cancel</button>" +
-                "</span>" +
-                "</div>");
-
-            var editTextArea = todoNote.find(".edit_note");
-
-            editTextArea.val(text);
-            editTextArea.focus();
-            resize(editTextArea);
-
-            editTextArea.on("input", function () {
-                resize($(this));
+            this.items.push({
+                id: this.currentTodoId,
+                text: this.newTodoText,
             });
 
-            todoNote.find(".save_button").click(function () {
-                text = todoNote.find(".edit_note").val().trim();
+            this.newTodoText = "";
+            ++this.currentTodoId;
+        },
 
-                if (text.length === 0) {
-                    todoNote.remove();
-                    return;
-                }
+        deleteItem: function (item) {
+            this.items = this.items.filter(function (filteredItem) {
+                return filteredItem !== item;
+            })
+        },
 
-                setViewMode();
-            });
-
-            todoNote.find(".cancel_button").click(setViewMode);
-        }
-
-        function setViewMode() {
-            todoNote.html("<div class='note_block'>" +
-                "<span class='todo_note'></span>" +
-                "<span class='button_group'>" +
-                "<button class='list_button note_button edit_button' type='button'>edit</button>" +
-                "<button class='list_button note_button delete_button' type='button'>delete</button>" +
-                "</span>" +
-                "</div>");
-
-            todoNote.find(".todo_note").text(text);
-
-            todoNote.find(".delete_button").click(function () {
-                todoNote.remove();
-            });
-
-            todoNote.find(".todo_note").click(setEditMode);
-            todoNote.find(".edit_button").click(setEditMode);
-        }
-
-        setViewMode();
-        todoList.append(todoNote);
-        newTodoNote.val("");
-
-        function resize($textArea) {
-            $textArea.css("height", "auto");
-            $textArea.css("height", $textArea[0].scrollHeight + "px");
+        saveItem: function (item, newTodoText) {
+            item.text = newTodoText;
         }
     }
-
-    newTodoNote.keydown(function (event) {
-        var keyCode = (event.keyCode ? event.keyCode : event.which);
-
-        if (keyCode === 13) {
-            event.preventDefault();
-            addNewNoteHandler();
-        }
-    });
-
-    addNewNoteButton.click(addNewNoteHandler);
-});*/
+});
