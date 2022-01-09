198af8f32a955ab5e32ba06892f5ebba927d5ace
bilutama <Borntobewild282>
2022-01-09 18:27:08 +0700

    todo_list_vue

--- Todo_list_vue/autoresize.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    function resize($textArea) {
-        $textArea.css("height", "auto");
-        $textArea.css("height", $textArea[0].scrollHeight + "px");
-    }
-
-
-});
