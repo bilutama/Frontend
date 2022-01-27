c9e45ad9636a6434f7ab32962c1f514775c82a21
Maksim Biluta <biluta.m.a@gmail.com>
2022-01-27 09:00:03 +0100

    vuetify

--- vuetify_project/src/movieDbService.js
+++ vuetify_project/src/movieDbService.js
@@ -4,7 +4,7 @@ import qs from "qs";
 
 export default class {
     constructor() {
-        this.apiUrl = "https://api.themoviedb.org/3";
+        this.host = "https://api.themoviedb.org/3";
         this.getPopularUrl = "/movie/popular";
         this.searchUrl = "/search/movie";
         this.genresUrl = "/genre/movie/list";
@@ -13,26 +13,64 @@ export default class {
         this.apiKeyValue = apiKey.apiKey;
     }
 
-    get(url, data) {
+    get(url, query) {
         return axios.get(url, {
-                params: data
+                params: query
             }
         )
-    };
-
-    post(url, data) {
-        return axios.post(url, this.qs.stringify(data));
     }
 
-    getContacts(term) {
-        return this.get(this.apiUrl + "getContacts", {term});
+    post(url, query) {
+        return axios.post(url, qs.stringify(query));
     }
 
-    addContact(contact) {
-        return this.post(this.apiUrl + "addContact", contact);
+    getPopularMovies(page) {
+        return this.get(this.host + this.getPopularUrl, {
+            api_key: this.apiKeyValue,
+            language: "en-US",
+            page: page
+        });
     }
 
-    deleteContact(ids) {
-        return this.post(this.apiUrl + "deleteContact", {ids});
+    /* getPopularMovies() - response object structure:
+    adult: false
+    backdrop_path: "/1Rr5SrvHxMXHu5RjKpaMba8VTzi.jpg"
+    genre_ids: [28, 12, 878]
+    id: 634649
+    original_language: "en"
+    original_title: "Spider-Man: No Way Home"
+    overview: "Peter Parker is unmasked and ..."
+    popularity: 9425.322
+    poster_path: "/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg"
+    release_date: "2021-12-15"
+    title: "Spider-Man: No Way Home"
+    video: false
+    vote_average: 8.5
+    vote_count: 5898
+    */
+
+    getMoviesBySearch(term, page) {
+        return this.get(this.host + this.searchUrl, {
+            api_key: this.apiKeyValue,
+            language: "en-US",
+            query: term,
+            page: page
+            /* optional:
+            include_adult
+            region
+            year
+            primary_release_year
+            */
+        });
     }
+
+    //
+    // getMovieGenres(movieId) {
+    //     // TODO
+    //     return null;
+    // }
+    //
+    // getMovieDetails(movieId) {
+    //     return null;
+    // }
 }
