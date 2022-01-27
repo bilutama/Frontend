e7e27917b116b0bb4bb04d669aaa851c1a76ee0f
biluta.ma <biluta.m.a@gmail.com>
2022-01-27 21:02:54 +0100

    vuetify

--- vuetify_project/src/movieDbService.js
+++ vuetify_project/src/movieDbService.js
@@ -4,12 +4,13 @@ import qs from "qs";
 
 export default class {
     constructor() {
-        this.host = "https://api.themoviedb.org/3";
+        this.hostApi = "https://api.themoviedb.org/3";
         this.getPopularUrl = "/movie/popular";
         this.searchUrl = "/search/movie";
         this.genresUrl = "/genre/movie/list";
         this.movieDetailsUrl = "/movie";
         this.movieRecommendationsUrl = "/recommendations"; // Full url example: /movie/{movie_id}/recommendations
+        this.imageSourceBaseUrl = "https://image.tmdb.org/t/p/";
         this.apiKeyValue = apiKey.apiKey;
     }
 
@@ -25,7 +26,7 @@ export default class {
     }
 
     getPopularMovies(page) {
-        return this.get(this.host + this.getPopularUrl, {
+        return this.get(this.hostApi + this.getPopularUrl, {
             api_key: this.apiKeyValue,
             language: "en-US",
             page: page
@@ -34,23 +35,23 @@ export default class {
 
     /* getPopularMovies() - response object structure:
     adult: false
-    backdrop_path: "/1Rr5SrvHxMXHu5RjKpaMba8VTzi.jpg"
+    backdrop_path: "/backdrop_image_name.jpg"
     genre_ids: [28, 12, 878]
     id: 634649
     original_language: "en"
-    original_title: "Spider-Man: No Way Home"
-    overview: "Peter Parker is unmasked and ..."
+    original_title: "Original movie title text"
+    overview: "some text"
     popularity: 9425.322
-    poster_path: "/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg"
+    poster_path: "/image_name.jpg"
     release_date: "2021-12-15"
-    title: "Spider-Man: No Way Home"
+    title: "Movie title"
     video: false
     vote_average: 8.5
     vote_count: 5898
     */
 
     getMoviesBySearch(term, page) {
-        return this.get(this.host + this.searchUrl, {
+        return this.get(this.hostApi + this.genresUrl, {
             api_key: this.apiKeyValue,
             language: "en-US",
             query: term,
@@ -64,11 +65,17 @@ export default class {
         });
     }
 
-    //
-    // getMovieGenres(movieId) {
-    //     // TODO
-    //     return null;
-    // }
+    getMovieGenres(movieId) {
+        return this.get(this.hostApi + this.searchUrl, {
+            api_key: this.apiKeyValue,
+            language: "en-US",
+            query: term,
+            page: page
+            /* optional:
+            language:
+            */
+        });
+    }
     //
     // getMovieDetails(movieId) {
     //     return null;
